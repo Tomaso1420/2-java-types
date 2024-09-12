e819303739a46a8d0ee20a40180228b4dd070b1c
Tomaso1420 <Artem82005@mail.ru>
2024-09-12 12:38:54 +0500

    Марциохо Артем задание 4

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -4,18 +4,29 @@ public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        float result;
+        if (operation == "+") {
+            result = a + b;
+            return result;
+        } else if (operation == "-") {
+            result = a - b;
+            return result;
+        } else if (operation == "*") {
+            result = a * b;
+            return result;
+        } else if (operation == "/") {
+            result = (float) a / (float) b;
+            return result;
+        } else {
+            System.out.println("Wrong operation");
+            return 0;
+        }
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
     }
 
 }
